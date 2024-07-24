5d20d8d9150f68cfdeb583c163c19bd6ff62791e
ArtemBarkov <artemka.barik@gmail.com>
2024-07-24 21:08:56 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var=('apple',0,True)
+print(immutable_var)
+#значения элементов кортежа нельзя изменять так как кортежи неизменятся
+mutable_list=['1',0,True]
+mutable_list[0]=3
+print(mutable_list)
